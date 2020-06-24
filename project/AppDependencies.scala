dca6c66c7aaebaf94f18938bc7780fe3fcaf878f
Max Worgan <1410832+MaxWorgan@users.noreply.github.com>
2020-06-24 14:58:06 +0100

    AIV-2037 - dependecy updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,18 +4,18 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"           % "1.3.0"
+    "uk.gov.hmrc"             %% "bootstrap-play-26"           % "1.9.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %%  "bootstrap-play-26"          % "1.3.0"               % Test classifier "tests",
-    "uk.gov.hmrc"             %%  "service-integration-test"   % "0.9.0-play-26"       % "it",
-    "com.github.tomakehurst"  %   "wiremock-jre8"              % "2.22.0"              % "it",
+    "uk.gov.hmrc"             %%  "bootstrap-play-26"          % "1.9.0"               % Test classifier "tests",
+    "uk.gov.hmrc"             %%  "service-integration-test"   % "0.12.0-play-26"       % "it",
+    "com.github.tomakehurst"  %   "wiremock-jre8"              % "2.26.3"              % "it",
     "org.scalatest"           %%  "scalatest"                  % "3.0.8"               % "test",
     "com.typesafe.play"       %%  "play-test"                  % current               % "test",
-    "org.mockito"             %   "mockito-core"               % "3.1.0"               % "test",
+    "org.mockito"             %   "mockito-core"               % "3.3.3"               % "test",
     "org.pegdown"             %   "pegdown"                    % "1.6.0"               % "test, it",
-    "org.scalatestplus.play"  %%  "scalatestplus-play"         % "3.1.2"               % "test, it"
+    "org.scalatestplus.play"  %%  "scalatestplus-play"         % "3.1.3"               % "test, it"
   )
 
 }
